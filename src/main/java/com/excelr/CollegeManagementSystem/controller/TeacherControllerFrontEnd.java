dc67052d7662517e05d90188df8080d186fa4689
uysolanki <uysolanki@gmail.com>
2025-09-05 20:05:53 +0530

    Create using UI

--- src/main/java/com/excelr/CollegeManagementSystem/controller/TeacherControllerFrontEnd.java
+++ src/main/java/com/excelr/CollegeManagementSystem/controller/TeacherControllerFrontEnd.java
@@ -1,22 +1,58 @@
 package com.excelr.CollegeManagementSystem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.excelr.CollegeManagementSystem.model.Teacher;
+import com.excelr.CollegeManagementSystem.service.TeacherService;
 
 @Controller //return a webpage, which we will see on web browser
 public class TeacherControllerFrontEnd {
+	@Autowired
+	TeacherService teacherService;
 	
 	@GetMapping("/test1")
-	public String test()
+	public String test(Model model)
 	{
+		String name="Rohit";
+		
+		Teacher teacher=new Teacher(10,"Sachin",45,900);
+		model.addAttribute("teacher1",teacher);
+		return "test";
+	}
+	
+	
+	@GetMapping("/test2")
+	public String test2(ModelMap modelmap)
+	{
+		String name="Rohit";
+		
+		Teacher teacher=new Teacher(10,"Sachin",45,900);
+		modelmap.addAttribute("player",name);
+		modelmap.addAttribute("teacher1",teacher);
 		return "test";
 	}
 	
 	
 	@GetMapping("/teacherRegistrationForm")
-	public String teacherRegistrationForm()
+	public String teacherRegistrationForm(Model model)
 	{
+		Teacher teacher=new Teacher();
+		model.addAttribute("teacher",teacher);
 		return "teacher-registration-form";
+	}
+	
+	
+	@GetMapping("/readTeacherDetailsFromUI")
+	public String readTeacherDetailsFromUI(@ModelAttribute Teacher teacher)
+	{
+		Teacher teacher1=teacherService.saveTeacher(teacher);
+//		System.out.println(teacher);
+		return "confirm";
 	}
 
 }
