81c55f2944b735c35689214b4d4eb054b105bf66
uysolanki <uysolanki@gmail.com>
2025-09-05 20:42:35 +0530

    Create using UI

--- src/main/java/com/excelr/CollegeManagementSystem/controller/TeacherControllerFrontEnd.java
+++ src/main/java/com/excelr/CollegeManagementSystem/controller/TeacherControllerFrontEnd.java
@@ -1,11 +1,15 @@
 package com.excelr.CollegeManagementSystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.excelr.CollegeManagementSystem.model.Teacher;
 import com.excelr.CollegeManagementSystem.service.TeacherService;
@@ -52,7 +56,24 @@ public class TeacherControllerFrontEnd {
 	{
 		Teacher teacher1=teacherService.saveTeacher(teacher);
 //		System.out.println(teacher);
-		return "confirm";
+		return "redirect:/showAllTeachers";
+	}
+	
+	
+	@GetMapping("/showAllTeachers")
+	public String showAllTeachers(Model model)
+	{
+		List<Teacher> teachers=teacherService.getAllTeachers();
+		model.addAttribute("teachers",teachers);
+//		System.out.println(teacher);
+		return "allteachers";
+	}
+	
+	@RequestMapping("/deleteTeacherFE/{tno}")
+	public String deleteTeacher(@PathVariable("tno") int tno)
+	{
+		teacherService.deleteTeacher(tno);
+		return "redirect:/showAllTeachers";
 	}
 
 }
